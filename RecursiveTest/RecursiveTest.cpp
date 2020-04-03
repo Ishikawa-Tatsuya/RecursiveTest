baa85a248043a96d987e04bf6b43d2ddfdc1e365
Tatsuya Ishikawa <ishikawa-tatsuya@codeer.co.jp>
2020-04-03 14:49:58 +0900

    問題追加

--- RecursiveTest/RecursiveTest.cpp
+++ RecursiveTest/RecursiveTest.cpp
@@ -1,16 +1,39 @@
 ﻿#include "pch.h"
 #include <iostream>
 
-#define TEXT_BUF_LENGTH (sizeof(char) * 128)
+struct Data
+{
+	char text;
+};
+
+void arrayTest()
+{
+	Data* array;
+	int arrrayCount = 5;
+
+	{
+		array = (Data*)malloc(sizeof(Data) * arrrayCount);
+		array[0].text = '@';
+		array[1].text = ' ';
+		array[2].text = ' ';
+		array[3].text = ' ';
+		array[4].text = '@';
+	}
+
+	//textに@が入っているものを数えてください。
+
+	//メモリ解放はこうやるよ
+	free(array);
+}
 
 struct Tree
 {
-	char* text;
+	char text;
 	int childrenCount;
 	Tree* children;
 };
 
-int main()
+void treeTest()
 {
 	Tree* root;
 
@@ -22,39 +45,40 @@ int main()
 		Tree* node1_1;
 
 		root = (Tree*)malloc(sizeof(Tree));
-		root->text = (char*)malloc(TEXT_BUF_LENGTH);
-		strncpy_s(root->text, TEXT_BUF_LENGTH, "★", TEXT_BUF_LENGTH);
+		root->text = '@';
 		root->childrenCount = 3;
 		root->children = (Tree*)malloc(sizeof(Tree) * root->childrenCount);
 
 		node0 = &root->children[0];
-		node0->text = (char*)malloc(TEXT_BUF_LENGTH);
-		strncpy_s(node0->text, TEXT_BUF_LENGTH, "☆", TEXT_BUF_LENGTH);
+		node0->text = ' ';
 		node0->childrenCount = 0;
 		node0->children = nullptr;
 
 		node1 = &root->children[1];
-		node1->text = (char*)malloc(TEXT_BUF_LENGTH);
-		strncpy_s(node1->text, TEXT_BUF_LENGTH, "☆", TEXT_BUF_LENGTH);
+		node1->text = ' ';
 		node1->childrenCount = 1;
 		node1->children = (Tree*)malloc(sizeof(Tree) * node1->childrenCount);
 
 		node2 = &root->children[2];
-		node2->text = (char*)malloc(TEXT_BUF_LENGTH);
-		strncpy_s(node2->text, TEXT_BUF_LENGTH, "★", TEXT_BUF_LENGTH);
+		node2->text = '@';
 		node2->childrenCount = 0;
 		node2->children = nullptr;
 
 		node1_1 = &node1->children[0];
-		node1_1->text = (char*)malloc(TEXT_BUF_LENGTH);
-		strncpy_s(node1_1->text, TEXT_BUF_LENGTH, "★", TEXT_BUF_LENGTH);
+		node1_1->text = '@';
 		node1_1->childrenCount = 0;
 		node1_1->children = nullptr;
 	}
 
-	//textに★が入っている数を数えてください。
+	//textに@が入っている数を数えてください。
 
 
 	//メモリリークが起きないようにしてください。
 }
 
+
+int main()
+{
+	arrayTest();
+	return 0;
+}
